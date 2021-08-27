cae33b99920b53a2ca573aaa3677443e11504811
Luisc13 <luisinho0903@gmail.com>
2021-08-27 15:41:12 -0300

    Ultima aula - Surface Exploration

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import { NewRoom } from "./pages/NewRoom";
 import {AuthContextProvider} from './contexts/AuthContexts';
 import { Room } from "./pages/Room";
+import { AdminRoom } from "./pages/AdminRoom";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" exact component={Home}></Route>
           <Route path="/rooms/new" component={NewRoom}></Route>
           <Route path="/rooms/:id" component={Room}></Route>
+          <Route path="/admin/rooms/:id" component={AdminRoom}></Route>
       </Switch>
     </AuthContextProvider>
     </BrowserRouter>
